71ce4de9263d80059a5c3a7499e7836cd772595d
Txema-Lobo <90839729+Txema-Lobo@users.noreply.github.com>
2023-02-02 10:12:43 +0100

    Update and rename script2.sh to script3.sh

--- script2.sh
+++
@@ -1,1 +0,0 @@
-sudo wget -O Forwarder_AMA_installer.py https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/Syslog/Forwarder_AMA_installer.py&&sudo python Forwarder_AMA_installer.py
---
+++ script3.sh
@@ -0,0 +1,1 @@
+sudo wget -O cef_AMA_troubleshoot.py https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/CEF/cef_AMA_troubleshoot.py&&sudo python cef_AMA_troubleshoot.py
