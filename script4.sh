012c4e8c4f16b800982b5d3a64f6cc6fbfadf212
Txema-Lobo <90839729+Txema-Lobo@users.noreply.github.com>
2023-02-02 10:23:39 +0100

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,1 +1,2 @@
 logger -p local4.warn -P 514 -n 127.0.0.1 --rfc3164 -t CEF "0|Mock-test|MOCK|common=event-format-test|end|TRAFFIC|1|rt=$common=event-formatted-receive_time"
+echo -n "<164>CEF:0|Mock-test|MOCK|common=event-format-test|end|TRAFFIC|1|rt=$common=event-formatted-receive_time" | nc -u -w0 localhost 514
